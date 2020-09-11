8461b9ab1102d5962be761bafe3b9a5d6e7f1b7a
spazdrum <spazdrum@gmail.com>
2020-09-10 22:39:50 -0400

    added .env

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const express = require("express");
-require('dotenv').config();
+require("dotenv").config();
 const pRouter = require("./router");
 const server = express();
-const port = process.env.port || 5000 ;
+const port = process.env.PORT || 5000;
 
 server.use(express.json());
 server.use(pRouter);
